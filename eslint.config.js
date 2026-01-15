e052552d470a1ecb7a60a1f1d9126d6e68d3c61c
Raman Paulau <raman.paulau@axure.com>
2026-01-14 18:05:43 -0800

    demonstrate ESLint 'extends' failure

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,53 @@
 import antfu from '@antfu/eslint-config'
+import tailwindcss from 'eslint-plugin-better-tailwindcss'
+import { defineConfig } from 'eslint/config'
+import eslintParserVue from 'vue-eslint-parser'
 
-export default antfu({
-  vue: true,
-})
+/*
+  Following does't work with `antfu` config.
+  [error] ConfigError: Config (unnamed): Key "extends": This appears to be in eslintrc format rather than flat config format.
+ */
+const twConfigFlatExtends = {
+  files: ['**/*.vue'],
+  // This way I can use all "recommended" rules from the plugin.
+  // But still override single rule as needed. No need to spread rules, ESLint will handle merging.
+  extends: [
+    tailwindcss.configs.recommended,
+  ],
+  rules: {
+    'better-tailwindcss/enforce-shorthand-classes': ['off'],
+  },
+  settings: {
+    'better-tailwindcss': {
+      entryPoint: 'src/styles.css',
+    },
+  },
+  languageOptions: {
+    parser: eslintParserVue,
+  },
+}
+export default antfu({}, twConfigFlatExtends)
+
+// However the same config works with ESLint `defineConfig`.
+// export default defineConfig(twConfigFlatExtends)
+
+/*
+  But this works with `antfu` config.
+  Notice no `extends` key, instead rules are spread directly.
+ */
+// const twConfigFlatWithPlugins = {
+//   files: ['**/*.vue'],
+//   plugins: {
+//     'better-tailwindcss': tailwindcss,
+//   },
+//   rules: {
+//     ...tailwindcss.configs.recommended.rules,
+//     'better-tailwindcss/enforce-shorthand-classes': ['off'],
+//   },
+//   settings: {
+//     'better-tailwindcss': {
+//       entryPoint: 'src/styles.css',
+//     },
+//   },
+// }
+// export default antfu({}, twConfigFlatWithPlugins)
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
+  <!-- Wrong classes order to trigger "enforce-consistent-class-order" warning -->
+  <!-- and "w-4 h-4" to indicate "enforce-shorthand-classes" is turned off per config -->
+  <h1 class="rounded-lg bg-red-400 p-4 text-white text-xl w-4 h-4">
+    You did it!
+  </h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
 </template>
-
-<style scoped></style>
